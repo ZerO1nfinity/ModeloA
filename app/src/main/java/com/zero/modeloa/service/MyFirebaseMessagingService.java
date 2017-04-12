1068b3b8d527a66f2262ba5878a3851dc975374a
ZerO1nfinity <Meza.Hinostroza.Robin@gmail.com>
2017-04-12 18:48:17 -0500

    Integracion con Firebase y notificaciones

--- app/src/main/java/com/zero/modeloa/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/zero/modeloa/service/MyFirebaseMessagingService.java
@@ -1,9 +1,19 @@
 package com.zero.modeloa.service;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.zero.modeloa.LoginActivity;
+import com.zero.modeloa.R;
+import com.zero.modeloa.view.ContainerActivity;
 
 /**
  * Created by ROBIN on 06/04/2017.
@@ -14,6 +24,31 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         Log.e("FIREBASE", remoteMessage.getNotification().getBody());
-        Log.e("FIREBASE", remoteMessage.getData().toString());
+
+        betterNotication(remoteMessage);
+    }
+
+    public void betterNotication(RemoteMessage remoteMessage){
+        //Configurando el Intent
+        Intent intent = new Intent(this, ContainerActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 , intent, PendingIntent.FLAG_ONE_SHOT);
+
+        //Seleccionando el Ringtone y el icono
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        int icon = R.drawable.icon_fire;
+
+        //Construyendo nuestro Widget
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(icon)
+                .setContentTitle(remoteMessage.getFrom())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        //Intenciando nuestro Widget
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0 , notificationBuilder.build());
     }
 }
